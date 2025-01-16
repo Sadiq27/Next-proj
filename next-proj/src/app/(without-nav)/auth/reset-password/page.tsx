ed34bd23006bfcb5b56830f5aac596fd3385d79a
Sadiq <samedov.sadiq55@gmail.com>
2025-01-16 16:08:04 +0400

    Add New Pages for Routing

---
+++ next-proj/src/app/(without-nav)/auth/reset-password/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ResetPassword() {
+  return (
+    <div className="flex h-screen">
+      Reset
+    </div>
+  );
+}
---
+++ next-proj/src/app/(without-nav)/auth/signup/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function SignUp() {
+  return (
+    <div className="flex h-screen">
+      SignUp
+    </div>
+  );
+}
